202893b67c341fe2fac8dbc9b04b6ebc133194a3
Cyprien <cyprien.gille@orange.fr>
2023-06-17 23:59:07 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import { invoke } from "@tauri-apps/api/tauri";
+
+  let current_page_id = 0;
+</script>
+
+<main />
---
+++ src/lib/Landing.svelte
@@ -0,0 +1,3 @@
+<script lang="ts">
+    import { invoke } from "@tauri-apps/api/tauri";
+</script>
