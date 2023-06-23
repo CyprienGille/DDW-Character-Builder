dfac71d55d09eaf33ec6efba5d9fb3c3db159c9b
CyprienGille <>
2023-06-24 00:32:30 +0200

    Basics

--- src/lib/Description.svelte
+++ src/lib/Description.svelte
@@ -2,4 +2,24 @@
   import { c } from "../stores";
 </script>
 
-<main>Background + Entrée des elems misc, allies, orgs, backstory etc etc</main>
+<main>
+  <div class="px-4 py-5 mb-2 bg-white border rounded-md shadow sm:px-6">
+    <h3 class="text-lg font-medium leading-6 text-gray-900">
+      Pick a name, and character creation options.
+    </h3>
+    <p class="max-w-2xl mt-1 text-sm text-gray-500">
+      Tip: You will always be able to modify your name in your character sheet
+      directly! So if you want to pick your lineage and class beforehand, just
+      proceed to the next step.
+    </p>
+  </div>
+  <div class="w-1/3 flex">
+    <input
+      class="rounded-md w-full py-2 px-2 my-4 ml-2 border-slate-400 border-2"
+      type="text"
+      name="CName"
+      id="cname"
+      bind:value={$c.name}
+    />
+  </div>
+</main>
